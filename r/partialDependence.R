385a27d9e44e52007e913a4f118d613cf5b59f9f
(U118378) <michael.mayer@mobi.ch>
2019-07-05 08:35:46 +0200

    added by argument ot partialDependence

--- r/partialDependence.R
+++ r/partialDependence.R
@@ -7,6 +7,7 @@
 #' @author Michael Mayer, \email{mayermichael79@gmail.com}
 #' @param explainer DALEX explainer.
 #' @param grid A named list of grid points.
+#' @param by A vector of column names used to additionally group the results. Like this, it would also be possible to generate ceteris paribus profiles.
 
 #' @return Data frame with partial dependence data.
 #' 
@@ -21,18 +22,21 @@
 #' 
 #' dat <- partialDependence(explainer, grid = list(Species = levels(iris$Species), Petal.Length = 1:6))
 #' ggplot(dat, aes(x = Petal.Length, y = predicted, group = Species, color = Species)) + geom_point()
+#' 
+#' dat <- partialDependence(explainer, grid = list(Petal.Length = 1:6), by = "Species")
+#' ggplot(dat, aes(x = Petal.Length, y = predicted, group = Species, color = Species)) + geom_point()
 #' }
-partialDependence <- function(explainer, grid) {
+partialDependence <- function(explainer, grid, by = NULL) {
   stopifnot(inherits(explainer, "explainer"), 
             is.list(grid), length(grid) >= 1, 
-            (v <- names(grid)) %in% colnames(explainer$data), 
+            c(by, (v <- names(grid))) %in% colnames(explainer$data), 
             !any(c("label", "predicted") %in% v))
   
   explainer$data %>% 
     select(-one_of(v)) %>% 
     crossing(expand.grid(grid)) %>% 
     mutate(predicted = with(explainer, link(predict_function(model, .)))) %>% 
-    group_by_at(v) %>% 
+    group_by_at(c(v, by)) %>% 
     summarize(predicted = mean(predicted, na.rm = TRUE)) %>% 
     ungroup() %>% 
     mutate(label = explainer$label)
